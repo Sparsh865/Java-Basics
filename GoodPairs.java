71aa8a985c3744da5ed8cc4575fc5953f34178de
Sparsh865 <sparsh865.be22@chitkara.edu.in>
2024-08-23 13:17:31 +0530

    good pairs

---
+++ GoodPairs.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class GoodPairs {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    System.out.println("Good pair: (" + i + ", " + j + ")");
+                }
+            }
+        }
+    }
+}
