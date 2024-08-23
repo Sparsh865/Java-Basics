9f2096b857d4163259aade3b84a3879fde2b39b8
Sparsh865 <sparsh865.be22@chitkara.edu.in>
2024-08-23 22:21:46 +0530

    TransposeDone

--- TransponseMatrix.java
+++ TransponseMatrix.java
@@ -1,10 +1,38 @@
 import java.util.*;
 
 public class TransponseMatrix {
+
+    public static int[][] transpose(int[][] matrix){
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int[][] transposeMatrix = new int[cols][rows];
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                transposeMatrix[j][i] = matrix[i][j];
+            }
+        }
+        return transposeMatrix;
+    }
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int rows = sc.nextInt();
         int cols = sc.nextInt();
-        
+        int[][] matrix = new int[rows][cols];
+
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        int [][] transposeMatrix = transpose(matrix);
+
+        for(int i=0;i<transposeMatrix.length;i++){
+            for(int j=0;j<transposeMatrix[i].length;j++){
+                System.out.print(transposeMatrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+
     }
 }
